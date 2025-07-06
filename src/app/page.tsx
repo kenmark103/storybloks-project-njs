c1f624e19f15c6fd655fc2a7de3ac9de5f3badb3
kenmark103 <kenmark103@gmail.com>
2025-07-06 12:46:39 +0300

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@ import { getStoryblokApi } from '@/lib/storyblok';
 import { StoryblokStory } from '@storyblok/react/rsc';
 
 export default async function Home() {
-const { data } = await fetchData();
+const storyblokApi = getStoryblokApi();
+const { data } = await storyblokApi.get(`cdn/stories/tour`, { version: 'published' });
 
 return (
 	<div className="page">
 		<StoryblokStory story={data.story} />
 	</div>
 );
-}
-
-export async function fetchData() {
-const storyblokApi = getStoryblokApi();
-return await storyblokApi.get(`cdn/stories/tour`, { version: 'published' });
 }
