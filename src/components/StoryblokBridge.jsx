29bba33aeaebe573cffbf4c9f6c16b6b618726ee
kenmark103 <kenmark103@gmail.com>
2025-07-07 15:02:51 +0300

    modified:   src/components/StoryblokBridge.jsx

--- src/components/StoryblokBridge.jsx
+++ src/components/StoryblokBridge.jsx
@@ -3,13 +3,11 @@
 import { useEffect } from "react";
 import { StoryblokBridge } from "@storyblok/js";
 
-export default function StoryblokBridge() {
+export default function StoryblokBridgeComponent() {
   useEffect(() => {
-    // Instantiate the bridge only if we're inside the Storyblok editor
     if (window.location.search.includes("_storyblok")) {
       const sbBridge = new StoryblokBridge();
 
-      // Reload the page on content change
       sbBridge.on(["change", "published", "input"], () => {
         window.location.reload();
       });
