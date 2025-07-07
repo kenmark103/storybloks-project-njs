f8f26e595255bf2b13a41d279df9e225e04aadbf
kenmark103 <kenmark103@gmail.com>
2025-07-07 19:27:37 +0300

    modified:   src/components/StoryblokBridge.jsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import StoryblokProvider from "@/components/StoryblokProvider";
+import StoryblokBridgeComponent from "@/components/StoryblokBridge"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-});
+});  
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -29,9 +29,9 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <StoryblokProvider>
             {children}
-          </StoryblokProvider>
+             <StoryblokBridgeComponent />
+         
         </body>
       </html>
     
--- src/components/StoryblokBridge.jsx
+++ src/components/StoryblokBridge.jsx
@@ -1,36 +1,24 @@
-"use client";
+"use client"
 
-import { useEffect } from "react";
-import { loadStoryblokBridge } from "@storyblok/js";
+import { useEffect } from "react"
+import { loadStoryblokBridge } from "@storyblok/js"
 
 export default function StoryblokBridgeComponent() {
   useEffect(() => {
-    if (window.location.search.includes("_storyblok")) {
-      loadStoryblokBridge(() => {
-        const { StoryblokBridge } = window;
-        console.log("✅ Storyblok Bridge loaded");
-        if (typeof StoryblokBridge !== "undefined") {
-          const sbBridge = new StoryblokBridge();
+    loadStoryblokBridge(() => {
+      const { StoryblokBridge } = window;
+      if (typeof StoryblokBridge !== "undefined") {
+        const bridge = new StoryblokBridge()
 
-          sbBridge.on(["change", "published", "input"], (event) => {
-            console.log("🟢 Storyblok event triggered:", event);
-            window.location.reload();
-          });
-        }
-      });
-    }
-    else{
-        console.log("❌ _storyblok query param missing — bridge not initialized.");
-    }
-  }, []);
-
-  return null;
-}
-
-export function initStoryblokBridge(){
-    loadStoryblokBridge().then(()=>{
-        if (typeof window.StoryblokBridge !== 'undefined'){
-            new window.StoryblokBridge();
-        }
+        bridge.on(["change", "published", "input"], (event) => {
+          console.log("🟢 Storyblok event triggered:", event)
+          // Instead of reload you can hot-update here too
+          location.reload()
+        })
+        console.log("✅ Storyblok Bridge initialized")
+      }
     })
+  }, [])
+
+  return null
 }
