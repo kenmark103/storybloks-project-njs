a3f17927aa30e805c3be6af24814731947b4b525
kenmark103 <kenmark103@gmail.com>
2025-07-07 15:05:50 +0300

    modified:   src/components/StoryblokBridge.jsx

--- src/components/StoryblokBridge.jsx
+++ src/components/StoryblokBridge.jsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useEffect } from "react";
-import { StoryblokBridge } from "@storyblok/js";
+import { StoryblokBridge as SbBridge } from "@storyblok/js";
 
 export default function StoryblokBridgeComponent() {
   useEffect(() => {
     if (window.location.search.includes("_storyblok")) {
-      const sbBridge = new StoryblokBridge();
+      const sbBridge = new SbBridge();
 
       sbBridge.on(["change", "published", "input"], () => {
         window.location.reload();
