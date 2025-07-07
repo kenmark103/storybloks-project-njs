463366e643b142ad7df7b7a23b0993753b444fd9
kenmark103 <kenmark103@gmail.com>
2025-07-07 15:04:50 +0300

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import StoryblokBridge from "@/components/StoryblokBridge";
+import StoryblokBridgeComponent from "@/components/StoryblokBridge";
 
 
 const geistSans = Geist({
@@ -37,7 +37,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           {children}
-           <StoryblokBridge />
+           <StoryblokBridgeComponent />
         </body>
       </html>
     
