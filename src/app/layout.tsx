d3b123921905a84de3920991f15ebccef2fb0404
Kenny <33131062+kenmark103@users.noreply.github.com>
2025-07-08 11:46:42 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,9 +26,9 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-        <heade>
+        <head>
           <script src="//app.storyblok.com/f/storyblok-v2-latest.js" async /> 
-        </heade>
+        </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
